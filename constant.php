633399c4ea5eebdafde1f3fbb08254fdc8d3fe53
adnan hadiyana <adnanhadiyana6@mail.unpas.ac.id>
2019-10-02 21:12:18 +0700

    membuat costanta

---
+++ constant.php
@@ -0,0 +1,28 @@
+<?php
+
+// define('NAMA', 'Adnan Hadiyana');
+// echo NAMA;
+// echo "<br>";
+// const UMUR = 21;
+// echo UMUR;
+
+
+// class Coba {
+//     const NAMA = 'Adnan Hadiyana';
+// }
+// echo Coba::NAMA;
+
+// echo __LINE__;
+// echo __FILE__;
+
+
+// function Coba(){
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+class Coba{
+    public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
