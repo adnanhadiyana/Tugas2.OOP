cbb285ce6cae637f7bbc1644ff9b1c42ebaabe1d
adnan hadiyana <adnanhadiyana6@mail.unpas.ac.id>
2019-10-02 22:27:32 +0700

    membuat namespace

---
+++ namespace/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'App/init.php';
+// $produk1 = new Komik("Naruto", "Masahi Kishimoto", "Shoen jump", 45000, 100, 0);
+// $produk2 = new Game("Uncharted", "Neil druckmann", "Sony Computer", 25000,50);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+
+
+new App\Produk\User();
