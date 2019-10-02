3d38929b3a50c7315dbe97da89d0cde01163c2b6
adnan hadiyana <adnanhadiyana6@mail.unpas.ac.id>
2019-10-02 22:05:05 +0700

    class baru info produk

---
+++ autoloading/App/init.php
@@ -0,0 +1,10 @@
+<?php
+// require_once 'Produk/InfoProduk.php';
+// require_once 'Produk/Produk.php';
+// require_once 'Produk/Komik.php';
+// require_once 'Produk/Game.php';
+// require_once 'Produk/CetakInfoProduk.php';
+
+spl_autoload_register(function($clas){
+require_once'Produk/' . $clas . '.php';
+});
---
+++ autoloading/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'App/init.php';
+$produk1 = new Komik("Naruto", "Masahi Kishimoto", "Shoen jump", 45000, 100, 0);
+$produk2 = new Game("Uncharted", "Neil druckmann", "Sony Computer", 25000,50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
