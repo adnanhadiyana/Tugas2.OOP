a9381dcb1401affc40a9529ef6b6a1a72d4e33d2
adnan hadiyana <adnanhadiyana6@mail.unpas.ac.id>
2019-10-02 21:01:42 +0700

    static keyword

--- static.php
+++ static.php
@@ -1,13 +1,35 @@
 <?php
-class ContohStatic{
+// class ContohStatic{
+//     public static $angka = 1;
+//     public static function halo(){
+//         return "Saya Cinta Hello Word." . self::$angka++ . " Kali";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+
+class Contoh{
     public static $angka = 1;
-    public static function halo(){
-        return "Saya Cinta Hello Word." . self::$angka++ . " Kali";
+
+    public function halo(){
+        return "Saya Cinta Hello Word." . self::$angka++ . " Kali. <br>";
     }
 }
+$obj = new Contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::halo();
 echo "<hr>";
-echo ContohStatic::halo();
+$obj2 = new Contoh;
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
